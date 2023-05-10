456fc8d45f9beda5b1992e168443f82be3b6fd23
NicklasAbrahamsen <95019758+NicklasAbrahamsen@users.noreply.github.com>
2023-05-10 09:45:36 +0200

    auktionshus

--- Models/Auktion.cs
+++ Models/Auktion.cs
@@ -7,6 +7,7 @@ public class Auction
     public Guid Id { get; set; }
 
     public string Title { get; set; }
+    public string Brand { get; set; }
     public string Description { get; set; }
     public DateTime StartTime { get; set; }
     public DateTime EndTime { get; set; }
