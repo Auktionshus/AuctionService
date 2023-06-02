31f0a14c7adb3e7c558d8880ac2d4f3135da8608
Michael H Sloth <michael.h.sloth@gmmail.com>
2023-06-02 12:26:40 +0200

    Last Commit

--- AuctionService/Models/Auction.cs
+++ AuctionService/Models/Auction.cs
@@ -5,9 +5,9 @@ using System.Collections.Generic;
 public class Auction
 {
     public Guid Id { get; set; }
-    public Item Item { get; set; }
-    public User HighestBidder { get; set; }
-    public List<Bid> Bids { get; set; }
+    public Item? Item { get; set; }
+    public User? HighestBidder { get; set; }
+    public List<Bid>? Bids { get; set; }
     public DateTime StartTime { get; set; }
     public DateTime EndTime { get; set; }
     public decimal StartingPrice { get; set; }
